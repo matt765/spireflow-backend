81b96b034e629325282a1fd235004d192544b2b4
matt765 <mateusz.wyrebek@gmail.com>
2023-11-19 16:30:25 +0100

    Add procfile & change port

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
